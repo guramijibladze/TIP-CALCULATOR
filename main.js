aedb840f489167d0a5ff013238be50e3fa6a574d
guramijibladze <guramijibladze@gmail.com>
2021-01-27 17:36:02 +0400

    up

--- main.js
+++ main.js
@@ -6,12 +6,28 @@ const totalAmount = document.getElementById('total-amount');
 const eachPersonsMoney = document.getElementById('each-person-money');
 const button = document.getElementById('button');
 
+
+
+// console.log(animationText.textContent);
+
 isHidden = HTMLElement.hidden;
 HTMLElement.hidden = true | false;
 
 
     button.addEventListener('click', function(){
-          setTimeout(function(){
+
+        if(totalMoney.value == "" || eachPerson.value == "" || select.value == "აირჩიეთ..."){
+            setTimeout(function(){
+                document.getElementById('warning').hidden = true;
+            }, 7000);
+
+            document.getElementById('warning').hidden = false;
+            return false;
+        }
+
+        myLoadAnimation();
+
+        setTimeout(function(){
             // console.log(totalMoney.value);
             percent.innerHTML = totalMoney.value * ( select.value / 100 );
             totalAmount.innerHTML = +totalMoney.value + +( totalMoney.value * ( select.value / 100 ));
@@ -29,9 +45,43 @@ HTMLElement.hidden = true | false;
         },9000);
     });
 
-
-// function myFunction(){
-//     console.log('gura');
-// }
-
     
+
+function myLoadAnimation(){
+    const animationText = document.getElementById('text-content');
+    document.getElementById('animation').hidden = false;
+
+    const strText = animationText.textContent;
+    const splitText = strText.split("");
+    animationText.textContent = "";
+    
+    for(let i = 0; i < splitText.length; i++){
+        animationText.innerHTML += "<span>" + splitText[i] + "</span>";
+    }
+    // console.log(splitText);
+    let char = 0;
+    let timer = setInterval(onTick, 100);
+    animationHide();
+    function onTick(){
+        // console.log(splitText);
+        const span = animationText.querySelectorAll('span')[char];
+        span.classList.add('fade');
+        char++;
+        if(char === splitText.length){
+            complete(timer);
+            return;
+        }
+    }
+
+    function animationHide(){
+        setTimeout(function(){
+            document.getElementById('animation').hidden = true;
+        },2000);
+    }
+    // setInterval(onTick, 50);
+}
+
+    function  complete(timer){
+        clearInterval(timer);
+        timer = null;
+    }
